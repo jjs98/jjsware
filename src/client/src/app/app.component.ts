f3b579fb74aefc945eca63b1a808a2764245e4c1
Jens Schmitz <jens.schmitz@serviceware.de>
2023-01-20 13:29:28 +0100

    update to pnpm and fix styles

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
   public availableLanguages: string[] = [this.english, this.german];
 
   public currentLanguage: string;
+  public isDarkTheme: boolean = true;
 
   constructor(private _translate: TranslateService) {
     this.currentLanguage = this.english;
@@ -34,8 +35,31 @@ export class AppComponent {
     return this.isMobileView() ? 'over' : 'side';
   }
 
+  public getTheme(): string {
+    var theme = localStorage.getItem('theme');
+    if (theme == 'light-theme') {
+      this.isDarkTheme = false;
+      return theme;
+    }
+    else
+    {
+      this.isDarkTheme = true;
+      return 'dark-theme';
+    }
+  }
+  
+  public switchTheme(): void {
+    var theme = this.getTheme();
+    if (theme == 'dark-theme'){
+      localStorage.setItem('theme', 'light-theme');
+    }
+    else {
+      localStorage.setItem('theme', 'dark-theme');
+    }
+  }
+
   public navItemClicked(): void {
-    if (this.getMode() == 'over'){
+    if (this.getMode() == 'over') {
       this.sidenav.close();
     }
   }
